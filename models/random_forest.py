e2c9fd35b1fd2008aa16ea966aec6b21fcdb63b6
robertwiles123 <133101861+robertwiles123@users.noreply.github.com>
2023-07-13 13:15:22 +0000

    File to fun all models at once. Updated error in decistion tree

--- models/random_forest.py
+++ models/random_forest.py
@@ -1,112 +1,103 @@
-from grade_packages import encoding
-import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score, mean_squared_error
-from sklearn.model_selection import train_test_split, KFold, cross_val_score, learning_curve
+from sklearn.model_selection import KFold, cross_val_score, learning_curve
 import matplotlib.pyplot as plt
 from joblib import dump
 # from sklearn.model_selection import GridSearchCV
 
-file_name = input('What file do you want to test? ')
-learning_grades = pd.read_csv('../csv_clean/' + file_name + '.csv')
+def random_forest(X, y, X_train, X_test, y_train, y_test, type_science, encoder, file_name):
 
-type_science = input('Is it triple or combined? ')
+    if type_science == 'c':
+        y_train = np.ravel(y_train)
+        y_test = np.ravel(y_test)
+        y = np.ravel(y)
 
-encoder, X, y = encoding.one_hot_fit(learning_grades, type_science)
+    """
+    param_grid = {
+        'max_depth': [3, 5, 7, 10],
+        'n_estimators': [50, 100, 200],
+        'min_samples_split': [2, 5, 10],
+        'min_samples_leaf': [1, 2, 4],
+        'max_features': ['sqrt', 'log2'],
+        'max_samples': [None, 0.5, 0.8],
+        'bootstrap': [True, False],
+        'random_state': [42]
+    }
 
-if type_science.lower()[0] == 'c':
-    y = y.values.ravel()
+    # Starting model
+    rf_model = RandomForestRegressor()
 
-# split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    # Perform grid search with cross-validation
+    grid_search = GridSearchCV(rf_model, param_grid, cv=5, scoring='neg_mean_squared_error')
+    grid_search.fit(X_train, y_train)
 
-"""
-param_grid = {
-    'max_depth': [3, 5, 7, 10],
-    'n_estimators': [50, 100, 200],
-    'min_samples_split': [2, 5, 10],
-    'min_samples_leaf': [1, 2, 4],
-    'max_features': ['sqrt', 'log2'],
-    'max_samples': [None, 0.5, 0.8],
-    'bootstrap': [True, False],
-    'random_state': [42]
-}
+    # Print the best hyperparameters and corresponding score
+    print("Best hyperparameters:")
+    print(grid_search.best_params_)
+    print("Best Score (Negative MSE):", -grid_search.best_score_)
 
-# Starting model
-rf_model = RandomForestRegressor()
-
-# Perform grid search with cross-validation
-grid_search = GridSearchCV(rf_model, param_grid, cv=5, scoring='neg_mean_squared_error')
-grid_search.fit(X_train, y_train)
-
-# Print the best hyperparameters and corresponding score
-print("Best hyperparameters:")
-print(grid_search.best_params_)
-print("Best Score (Negative MSE):", -grid_search.best_score_)
-
-"""
-if type_science.lower()[0] == 'c':
-    rf = RandomForestRegressor(random_state=42, bootstrap=False, max_depth=10, max_features='sqrt', max_samples=None, min_samples_leaf=1, min_samples_split=2, n_estimators=100)
-elif type_science.lower()[0] == 't':
-    rf = RandomForestRegressor(bootstrap=False, max_depth=7, max_features='sqrt', max_samples=None, min_samples_leaf=1, min_samples_split=5, n_estimators=50, random_state=42)
-else:
-    print('No model selected')
-    
-
-rf.fit(X_train, y_train)
-
-y_pred_unrounded = rf.predict(X_test)
-
-y_pred = np.vectorize(lambda x: round(x * 2) / 2)(y_pred_unrounded)
-
-mse = mean_squared_error(y_test, y_pred)
-rmse = np.sqrt(mse)
-
-r2 = r2_score(y_test, y_pred)
-print("Mean Squared Error (MSE): {:.2f}".format(mse))
-# thows an error due to difference in size, needs to be looked at
-y_train_size = y_pred.shape[0]
-print('Train MSE {:.2f}'.format(mean_squared_error(y_train[:y_train_size], y_pred)))
-print('testMSE {:.2f}'.format(mean_squared_error(y_test, y_pred)))
-
-print("Root Mean Squared Error (RMSE): {:.2f}".format(rmse))
-print("R2 score:", r2)
-
-
-# The above R2 scoreis so hight I will be checking that the data is not overfit
-
-kfold = KFold(n_splits=10, shuffle=True, random_state=15)
-scores = cross_val_score(rf, X, y, cv=kfold, scoring='r2')
-# the cross-validated scores are very similar, reducing the chance that the model is overfitted
-print('Cross-validation scores:', scores)
-
-train_sizes, train_scores, test_scores = learning_curve(rf, X, y, train_sizes=np.linspace(0.1, 1.0, 10), cv=5)
-
-train_mean = np.mean(train_scores, axis=1)
-train_std = np.std(train_scores, axis=1)
-test_mean = np.mean(test_scores, axis=1)
-test_std = np.std(test_scores, axis=1)
-
-plt.plot(train_sizes, train_mean, color='blue', marker='o', markersize=5, label='training score')
-plt.fill_between(train_sizes, train_mean + train_std, train_mean - train_std, alpha=0.15, color='blue')
-plt.plot(train_sizes, test_mean, color='green', linestyle='--', marker='s', markersize=5, label='test score')
-plt.fill_between(train_sizes, test_mean + test_std, test_mean - test_std, alpha=0.15, color='green')
-plt.xlabel('Number of training samples')
-plt.ylabel('Score')
-plt.legend(loc='lower right')
-plt.show()
-plt.savefig("../model_graphs/" + file_name + "_random.png", )
-
-# It seems the model is good for making predictions if there are enough data points. I will continue to work on this if there is more data avalible
-
-save = input('should it be saved? ')
-if save[0].strip().lower() == 'y':
+    """
     if type_science.lower()[0] == 'c':
-        dump(rf, '../combined_models/combined_random_forest.joblib')
-        dump(encoder, '../combined_models/combined_random_forest_encoding.joblib')
-        print('Model save')
+        rf = RandomForestRegressor(random_state=42, bootstrap=False, max_depth=10, max_features='sqrt', max_samples=None, min_samples_leaf=1, min_samples_split=2, n_estimators=100)
+    elif type_science.lower()[0] == 't':
+        rf = RandomForestRegressor(bootstrap=False, max_depth=7, max_features='sqrt', max_samples=None, min_samples_leaf=1, min_samples_split=5, n_estimators=50, random_state=42)
     else:
-        dump(rf, '../triple_models/triple_random_forest.joblib')
-        dump(encoder, '../triple_models/triple_random_forest_encoding.joblib')
-        print('Model save')
+        print('No model selected')    
+        
+    rf.fit(X_train, y_train)
+
+    y_pred_unrounded = rf.predict(X_test)
+
+    y_pred = np.vectorize(lambda x: round(x * 2) / 2)(y_pred_unrounded)
+
+    mse = mean_squared_error(y_test, y_pred)
+    rmse = np.sqrt(mse)
+
+    r2 = r2_score(y_test, y_pred)
+    print("Mean Squared Error (MSE): {:.2f}".format(mse))
+    # thows an error due to difference in size, needs to be looked at
+    y_train_size = y_pred.shape[0]
+    print('Train MSE {:.2f}'.format(mean_squared_error(y_train[:y_train_size], y_pred)))
+    print('testMSE {:.2f}'.format(mean_squared_error(y_test, y_pred)))
+
+    print("Root Mean Squared Error (RMSE): {:.2f}".format(rmse))
+    print("R2 score:", r2)
+
+
+    # The above R2 scoreis so hight I will be checking that the data is not overfit
+
+    kfold = KFold(n_splits=10, shuffle=True, random_state=15)
+    scores = cross_val_score(rf, X, y, cv=kfold, scoring='r2')
+    # the cross-validated scores are very similar, reducing the chance that the model is overfitted
+    print('Cross-validation scores:', scores)
+
+    train_sizes, train_scores, test_scores = learning_curve(rf, X, y, train_sizes=np.linspace(0.1, 1.0, 10), cv=5)
+
+    train_mean = np.mean(train_scores, axis=1)
+    train_std = np.std(train_scores, axis=1)
+    test_mean = np.mean(test_scores, axis=1)
+    test_std = np.std(test_scores, axis=1)
+
+    plt.plot(train_sizes, train_mean, color='blue', marker='o', markersize=5, label='training score')
+    plt.fill_between(train_sizes, train_mean + train_std, train_mean - train_std, alpha=0.15, color='blue')
+    plt.plot(train_sizes, test_mean, color='green', linestyle='--', marker='s', markersize=5, label='test score')
+    plt.fill_between(train_sizes, test_mean + test_std, test_mean - test_std, alpha=0.15, color='green')
+    plt.xlabel('Number of training samples')
+    plt.ylabel('Score')
+    plt.legend(loc='lower right')
+    plt.show()
+    plt.savefig("model_graphs/" + file_name + "_random.png", )
+
+    # It seems the model is good for making predictions if there are enough data points. I will continue to work on this if there is more data avalible
+
+    save = input('should it be saved? ')
+    if save[0].strip().lower() == 'y':
+        if type_science.lower()[0] == 'c':
+            dump(rf, 'combined_models/combined_random_forest.joblib')
+            dump(encoder, 'combined_models/combined_random_forest_encoding.joblib')
+            print('Model save')
+        else:
+            dump(rf, 'triple_models/triple_random_forest.joblib')
+            dump(encoder, 'triple_models/triple_random_forest_encoding.joblib')
+            print('Model save')
--- models/ridge.py
+++ models/ridge.py
@@ -6,121 +6,64 @@ from sklearn.model_selection import train_test_split, KFold, cross_val_score, le
 from sklearn.metrics import  r2_score, mean_squared_error
 import matplotlib.pyplot as plt
 from joblib import dump
-# import re
 
-file_name = input('What file do you want to test? ')
-learning_grades = pd.read_csv('../csv_clean/' + file_name + '.csv')
+def ridge(X, y, X_train, X_test, y_train, y_test, type_science, encoder, file_name):
+    # Create and train the Ridge regression model
+    model = Ridge(alpha=1.0)  # Adjust the alpha parameter as needed
+    model.fit(X_train, y_train)
 
-type_science = input('Is it triple or combined? ')
+    # Make predictions
+    y_pred_unrounded = model.predict(X_test)
 
-encoder, X, y = encoding.one_hot_fit(learning_grades, type_science)
+    y_pred = np.vectorize(lambda x: round(x * 2) / 2)(y_pred_unrounded)
 
-# code that confirmed that each of the variables are useful for combined classes
-"""
-ridge_cv = RidgeCV()
-
-# code to determine if any features aren't needed
-feature_scores = {}
-for column in X.columns:
-    X_array = X[[column]]
-    scores = cross_val_score(ridge_cv, X_array, y, scoring='neg_mean_squared_error', cv=5)
-    feature_scores[column] = np.mean(scores)
-
-# New dictionary to store combined categories and average MSE scores
-new_dict = {}
-
-# Regular expression patterns
-pattern_year = re.compile(r'^Year 10 Combined MOCK GRADE_')
-pattern_combined = re.compile(r'^Combined MOCK GRADE term 2_')
-pattern_fft = re.compile(r'^FFT20_')
-
-# Combine similar categories and calculate average MSE scores
-for feature, mse in feature_scores.items():
-    if pattern_year.match(feature):
-        category = 'Year 10 Combined MOCK GRADE'
-    elif pattern_combined.match(feature):
-        category = 'Combined MOCK GRADE term 2'
-    elif pattern_fft.match(feature):
-        category = 'FFT20'
-    else:
-        category = 'SEN bool'  # Default category
-        
-    if category not in new_dict:
-        new_dict[category] = []
-    
-    new_dict[category].append(mse)
-
-# Calculate average MSE scores for each category
-for category, scores in new_dict.items():
-    average_mse = sum(scores) / len(scores)
-    new_dict[category] = average_mse
-
-# Print the new dictionary with combined categories and average MSE scores
-for category, average_mse in new_dict.items():
-    print(f"Category: {category}, Average MSE Score: {average_mse}")
-"""
-
-# code to run when complete
-
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Create and train the Ridge regression model
-model = Ridge(alpha=1.0)  # Adjust the alpha parameter as needed
-model.fit(X_train, y_train)
-
-# Make predictions
-y_pred_unrounded = model.predict(X_test)
-
-y_pred = np.vectorize(lambda x: round(x * 2) / 2)(y_pred_unrounded)
-
-# evaluate the model performance using mean squred error, root and r squared
-mse = mean_squared_error(y_test, y_pred_unrounded)
-rmse = np.sqrt(mse)
-r2 = r2_score(y_test, y_pred_unrounded)
+    # evaluate the model performance using mean squred error, root and r squared
+    mse = mean_squared_error(y_test, y_pred_unrounded)
+    rmse = np.sqrt(mse)
+    r2 = r2_score(y_test, y_pred_unrounded)
 
 
-y_train_size = y_pred_unrounded.shape[0]
-print("Mean Squared Error (MSE): {:.2f}".format(mse))
-print('Train MSE {:.2f}'.format(mean_squared_error(y_train[:y_train_size], y_pred_unrounded)))
-print('testMSE {:.2f}'.format(mean_squared_error(y_test, y_pred_unrounded)))
-print("Root Mean Squared Error (RMSE): {:.2f}".format(rmse))
-print("R2 score:", r2)
+    y_train_size = y_pred_unrounded.shape[0]
+    print("Mean Squared Error (MSE): {:.2f}".format(mse))
+    print('Train MSE {:.2f}'.format(mean_squared_error(y_train[:y_train_size], y_pred_unrounded)))
+    print('testMSE {:.2f}'.format(mean_squared_error(y_test, y_pred_unrounded)))
+    print("Root Mean Squared Error (RMSE): {:.2f}".format(rmse))
+    print("R2 score:", r2)
 
-# this again has an extremely high R2 score, so may be over fit
+    # this again has an extremely high R2 score, so may be over fit
 
-# create a kfold to check scores are similar
-kfold = KFold(n_splits=10, shuffle=True, random_state=15)
-scores = cross_val_score(model, X, y, cv=kfold, scoring='r2')
-# the cross-validated scores are very similar, reducing the chance that the model is overfitted
-print('Cross-validation scores:', scores)
+    # create a kfold to check scores are similar
+    kfold = KFold(n_splits=10, shuffle=True, random_state=15)
+    scores = cross_val_score(model, X, y, cv=kfold, scoring='r2')
+    # the cross-validated scores are very similar, reducing the chance that the model is overfitted
+    print('Cross-validation scores:', scores)
 
-# create a learning curve to see if data is overfitting
-train_sizes, train_scores, test_scores = learning_curve(model, X, y, train_sizes=np.linspace(0.1, 1.0, 10), cv=5)
-                                                        
-train_mean = np.mean(train_scores, axis=1)
-train_std = np.std(train_scores, axis=1)
-test_mean = np.mean(test_scores, axis=1)
-test_std = np.std(test_scores, axis=1)
-                                                        
-plt.plot(train_sizes, train_mean, color='blue', marker='o', markersize=5, label='training score')
-plt.fill_between(train_sizes, train_mean + train_std, train_mean - train_std, alpha=0.15, color='blue')
-plt.plot(train_sizes, test_mean, color='green', linestyle='--', marker='s', markersize=5, label='test score')
-plt.fill_between(train_sizes, test_mean + test_std, test_mean - test_std, alpha=0.15, color='green')
-plt.xlabel('Number of training samples')
-plt.ylabel('Score')
-plt.legend(loc='lower right')
-plt.ylim([0, 1])
-plt.show()
-plt.savefig("../model_graphs/" + file_name + "_ridge.png", )
+    # create a learning curve to see if data is overfitting
+    train_sizes, train_scores, test_scores = learning_curve(model, X, y, train_sizes=np.linspace(0.1, 1.0, 10), cv=5)
+                                                            
+    train_mean = np.mean(train_scores, axis=1)
+    train_std = np.std(train_scores, axis=1)
+    test_mean = np.mean(test_scores, axis=1)
+    test_std = np.std(test_scores, axis=1)
+                                                            
+    plt.plot(train_sizes, train_mean, color='blue', marker='o', markersize=5, label='training score')
+    plt.fill_between(train_sizes, train_mean + train_std, train_mean - train_std, alpha=0.15, color='blue')
+    plt.plot(train_sizes, test_mean, color='green', linestyle='--', marker='s', markersize=5, label='test score')
+    plt.fill_between(train_sizes, test_mean + test_std, test_mean - test_std, alpha=0.15, color='green')
+    plt.xlabel('Number of training samples')
+    plt.ylabel('Score')
+    plt.legend(loc='lower right')
+    plt.ylim([0, 1])
+    plt.show()
+    plt.savefig("model_graphs/" + file_name + "_ridge.png", )
 
-save = input('should it be saved? ')
-if save[0].strip().lower() == 'y':
-    if type_science.lower()[0] == 'c':
-        dump(model, '../combined_models/combined_ridge.joblib')
-        dump(encoder, '../combined_models/combined_ridge_encoding.joblib')
-        print('Model save')
-    else:
-        dump(model, '../triple_models/triple_ridge.joblib')
-        dump(encoder, '../triple_models/triple_ridge_encoding.joblib')
-        print('Model save')
+    save = input('should it be saved? ')
+    if save[0].strip().lower() == 'y':
+        if type_science.lower()[0] == 'c':
+            dump(model, 'combined_models/combined_ridge.joblib')
+            dump(encoder, 'combined_models/combined_ridge_encoding.joblib')
+            print('Model save')
+        else:
+            dump(model, 'triple_models/triple_ridge.joblib')
+            dump(encoder, 'triple_models/triple_ridge_encoding.joblib')
+            print('Model save')
