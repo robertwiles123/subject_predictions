f16fc83ecaa433396c211acbc89e02d7b51ca38a
robertwiles123 <133101861+robertwiles123@users.noreply.github.com>
2023-09-04 10:32:07 +0000

    Fixed bug with p8 and upn changing to gender

--- grades_full_dirty/full_split.py
+++ grades_full_dirty/full_split.py
@@ -2,6 +2,9 @@ import pandas as pd
 import re
 import os
 
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+
 ap1 = pd.read_excel('Y11 2122 Ap1 MLG P8.xlsx', header=1)
 ap2 = pd.read_excel('Y11 2122 Ap2 MLG P8.xlsx', header=1)
 p8 = pd.read_excel('Y11 2122 FFT P8.xlsx', header=1)
@@ -11,20 +14,31 @@ def clean_column_names(df):
     df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '')
 
 dataframes = (ap1, ap2, p8)
-
 for df in dataframes:
     clean_column_names(df)
 
+#ensure upn for merge is clean
+for df in dataframes:
+    df['upn'] = df['upn'].str.strip().str.lower()
+    df.dropna(subset=['upn'], inplace=True)
+
 full = pd.merge(ap1, ap2, on='upn', how='outer', suffixes=('_ap1', '_ap2'))
 
-# give the p8 columns a suffix else won't have it on the join
+upn_index = p8.columns.get_loc('upn')
+
+# Move the 'upn' column to position 0
+p8 = p8[[p8.columns[upn_index]] + [col for col in p8.columns if col != 'upn']]
+
+# Give the remaining columns a suffix "_real"
 p8.columns = ['upn'] + [f'{col}_real' for col in p8.columns if col != 'upn']
+
+
 
 #merge real grades with the outcomes
 full = pd.merge(full, p8, on='upn', how='outer')
 
 #list of duplicated columns to drop after merge
-columns_to_drop = ['gender_ap1', 'gender_ap2', 'rm_scaled_score_ap1', 'rm_scaled_score_ap2', 'actual_ap1', 'actual_ap2', 'difference_ap1', 'difference_ap2', 'difference_real', 'entries_ap1', 'score_ap1', 'scoreadjusted_ap1', 'entries_ap2', 'score_ap2', 'scoreadjusted_ap2', 'entries_real', 'score_real']
+columns_to_drop = ['gender_ap1', 'rm_scaled_score_ap1', 'rm_scaled_score_ap2', 'actual_ap1', 'actual_ap2', 'difference_ap1', 'difference_ap2', 'difference_real', 'entries_ap1', 'score_ap1', 'scoreadjusted_ap1', 'entries_ap2', 'score_ap2', 'scoreadjusted_ap2', 'entries_real', 'score_real']
 
 # Drop the specified columns from the DataFrame
 full.drop(columns=columns_to_drop, inplace=True)
@@ -43,7 +57,7 @@ for subject in subjects:
     subject_pattern = f'{subject}.*'
     
     # Select columns using regex pattern and add common columns
-    columns_to_select = ['upn', 'rm_scaled_score_real', 'estimate_real', 'actual_real'] + [col for col in full.columns if re.search(subject_pattern, col)]
+    columns_to_select = ['upn', 'rm_scaled_score_real', 'estimate_real', 'actual_real', 'gender_ap2'] + [col for col in full.columns if re.search(subject_pattern, col)]
     
     # Create the DataFrame for the subject
     subject_df = full[columns_to_select]
---
+++ grades_split_dirty/cleaning.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+test = pd.read_csv('chemistry_df_2122.csv')
+
+print(test)
