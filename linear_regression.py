cc448184c97871a2903d7d3c08b08a2c8861619d
robertwiles123 <133101861+robertwiles123@users.noreply.github.com>
2023-09-05 13:23:49 +0000

    Updates to ridge model

--- linear_regression.py
+++ linear_regression.py
@@ -1,17 +1,9 @@
-"""
-Subjects that seems to be doing well are:
-"music_tech_grade," "biology," "history," "german," "d_&_t_textiles_technology," "geography," "business_studies," and "computer_science" 
-Though none are doing amazing
-"""
-import matplotlib.pyplot as plt
 import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 import re
-import numpy as np
-from sklearn.model_selection import KFold
 
 # does not include double science due to issue with data collection
 # Define a list of subjects
@@ -31,10 +23,9 @@ for root, dirs, files in os.walk(folder_path):
                 # Read the CSV file
                 csv_path = os.path.join(root, file)
                 df = pd.read_csv(csv_path)
-                df_unchanged = df.copy()
                 # List of categorical columns to be one-hot encoded (excluding 'UPN')
                 categorical_columns = [col for col in df.columns if col != 'upn']
-                
+
                 # Use pandas get_dummies to perform one-hot encoding
                 data_encoded = pd.get_dummies(df, columns=categorical_columns, drop_first=True) 
                 # Construct the regex pattern
@@ -43,6 +34,7 @@ for root, dirs, files in os.walk(folder_path):
                 matching_columns = data_encoded.columns
 
                 y_columns = [col for col in matching_columns if re.match(regex_pattern, col)]
+
                 # Filter the DataFrame based on the selected columns
                 y = data_encoded[y_columns]
 
@@ -51,11 +43,9 @@ for root, dirs, files in os.walk(folder_path):
                 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=27)
 
                 # Print the shapes of X_train and y_train for debugging
-                ridge_model = Ridge(alpha=0.5)      
-                ridge_model.fit(X_train, y_train)
-                y_pred = ridge_model.predict(X_test)
-                y_pred = np.round(y_pred)
-
+                linear_model = LinearRegression()  
+                linear_model.fit(X_train, y_train)
+                y_pred = linear_model.predict(X_test)
                 # Calculate Mean Squared Error (MSE)
                 mse = mean_squared_error(y_test, y_pred)
 
@@ -69,28 +59,6 @@ for root, dirs, files in os.walk(folder_path):
                 print(f"Mean Squared Error (MSE): {mse:.2f}")
                 print(f"Root Mean Squared Error (RMSE): {rmse:.2f}")
                 print(f"R-squared (R2) Score: {r2:.2f}")
-                # Initialize a KFold cross-validator with 5 folds
-                kf = KFold(n_splits=5)
-
-                mse_scores = []
-
-                # Perform cross-validation
-                for i, (train_index, test_index) in enumerate(kf.split(X)):
-                    
-                    # Fit the model on the training data
-                    ridge_model.fit(X_train, y_train)
-                    
-                    # Make predictions on the test data
-                    y_pred = ridge_model.predict(X_test)
-                    
-                    # Calculate the Mean Squared Error for this fold
-                    mse = mean_squared_error(y_test, y_pred)
-                    mse_scores.append(mse)
-
-                # Calculate the average MSE across all folds
-                average_mse = sum(mse_scores) / len(mse_scores)
-                print(f"Cross value score: {average_mse:.2f}")
                 print()
                 print()
-            
-            
+
