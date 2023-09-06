af435028966a1214bbfaa80129ecd56fb1b4069a
robertwiles123 <133101861+robertwiles123@users.noreply.github.com>
2023-09-06 13:15:48 +0000

    ridge saves models

--- ridge.py
+++ ridge.py
@@ -11,6 +11,8 @@ from sklearn.metrics import mean_squared_error, r2_score
 import re
 import numpy as np
 from sklearn.model_selection import KFold
+import joblib
+
 
 # does not include double science due to issue with data collection
 # Define a list of subjects
@@ -68,10 +70,6 @@ for root, dirs, files in os.walk(folder_path):
                 # Calculate R-squared (R2) score
                 r2 = r2_score(y_test, y_pred)
 
-                print(subject)
-                print(f"Mean Squared Error (MSE): {mse:.2f}")
-                print(f"Root Mean Squared Error (RMSE): {rmse:.2f}")
-                print(f"R-squared (R2) Score: {r2:.2f}")
                 # Initialize a KFold cross-validator with 5 folds
                 kf = KFold(n_splits=5)
 
@@ -92,6 +90,20 @@ for root, dirs, files in os.walk(folder_path):
 
                 # Calculate the average MSE across all folds
                 average_mse = sum(mse_scores) / len(mse_scores)
+                
+                print(subject)
+                print(f"Mean Squared Error (MSE): {mse:.2f}")
+                print(f"Root Mean Squared Error (RMSE): {rmse:.2f}")
+                print(f"R-squared (R2) Score: {r2:.2f}")
                 print(f"Cross value score: {average_mse:.2f}")
+                print()
+
+
+                model_filename = '/workspaces/subject_predictions/models/' + subject + '_ridge.joblib'
+
+                joblib.dump(ridge_model, model_filename)
+                
+                print('Model saved')
+                print()
                 print()
                 print()
