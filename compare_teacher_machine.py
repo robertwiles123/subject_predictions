53df07cc467a430baab5ad04228689e322b3b89d
Robert Wiles <robertwiles123@gmail.com>
2023-09-15 14:04:36 +0000

    Started code for comparing teacher and machine

--- compare_teacher_machine.py
+++ compare_teacher_machine.py
@@ -1,3 +1,12 @@
 import pandas as pd
 
-pd.
+subjects = ['art_&_design', 'biology', 'business_studies', 'chemistry', 'computer_science', 'drama', 'english_language', 'english_literature', 'food_technology', 'french_language', 'geography', 'german', 'history', 'maths', 'music_studies', 'physics', 'spanish']
+model = 'ridge'
+
+
+for subject in subjects:
+
+    prediction_df = pd.read_csv(f'{model}_predicted/{subject}_2223_prediction.csv')
+    prediction_df_basics = prediction_df[['upn', f'{subject}_prediction']]
+    
+    teacher_prediction = pd.read_csv()
