267fd3aa754467969db14dbc0c2a9dd80e431112
Robert Wiles <robertwiles123@gmail.com>
2023-09-18 08:35:28 +0000

    Draft for comparision of prediction and real. Awaiting real data

--- compare_teacher_machine.py
+++ compare_teacher_machine.py
@@ -9,4 +9,19 @@ for subject in subjects:
     prediction_df = pd.read_csv(f'{model}_predicted/{subject}_2223_prediction.csv')
     prediction_df_basics = prediction_df[['upn', f'{subject}_prediction']]
     
-    teacher_prediction = pd.read_csv()
+    teacher_prediction = pd.read_csv(f'to_be_predicted/{subject}_2223.csv')
+    teacher_filtered = teacher_prediction.filter(regex=f'{subject}.*_ap2$')
+
+    real_grades = pd.read_csv()
+
+    mae_prediction = abs(prediction_df_basics[f'{subject}_prediction'] - real_grades['real_grades']).mean()
+    print("Mean Absolute Error (MAE) prediction:", mae_prediction)
+
+    mse_prediction = ((prediction_df_basics[f'{subject}_prediction']  - real_grades['real_grades']) ** 2).mean()
+    print("Mean Squared Error (MSE) prediction:", mse_prediction)
+
+    mae_teacher = abs(teacher_prediction[teacher_filtered] - real_grades['real_grades']).mean()
+    print("Mean Absolute Error (MAE) prediction:", mse_teacher)
+
+    mse_teacher = ((teacher_prediction[teacher_filtered] - real_grades['real_grades']) ** 2).mean()
+    print("Mean Squared Error (MSE) prediction:", mse_teacher)
