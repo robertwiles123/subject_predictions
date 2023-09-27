b01ed82c10d5cf3215a3bf2763d8dfff296f62f6
Robert Wiles <robertwiles123@gmail.com>
2023-09-27 09:54:39 +0000

    Updated code so random forest uses correct params

--- subject_list.py
+++ subject_list.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def full_subjects():
     return ['art_&_design', 'biology', 'business_studies', 'chemistry', 'computer_science', 'd_&_t_product_design', 'd_&_t_textiles_technology', 'drama', 'english_language', 'english_literature', 'food_technology', 'french_language', 'geography', 'german', 'history', 'ict_btec', 'maths', 'music_studies', 'music_tech_grade', 'pearson_btec_sport', 'physics', 'product_design', 'science_double', 'spanish']
 
@@ -23,11 +25,23 @@ def get_model_name(x):
     elif 'Ridge' in x:
         return 'ridge'    
 
-def get_models(x):
+def get_models(x, name = None):
     # Check if RandomForestRegressor is imported, if yes, return 'random_forest', otherwise return 'linear_regression'
     if 'RandomForestRegressor' in x:
         from sklearn.ensemble import RandomForestRegressor
-        return RandomForestRegressor(random_state=25)
+        df = pd.read_csv('/workspace/subject_predictions/models/params.csv')
+        hyperparameters = {}
+        subject_row = df[df['subject'] == name]
+        if subject_row.empty or subject_row.isnull().values.any():
+            print(f"Hyperparameters for subject '{name}' not found in the CSV.")
+            model = RandomForestRegressor(max_depth= 5, n_estimators=100, min_samples_split=5, min_samples_leaf=2, max_features='sqrt',max_samples=0.5, bootstrap=False, random_state=42)
+        else:
+            # Extract hyperparameters as a dictionary
+            for column in df.columns:
+                value = subject_row[column].values[0]
+                if pd.notna(value) and column != 'subject':
+                    hyperparameters[column] = value
+        return RandomForestRegressor(**hyperparameters)
     elif 'LinearRegression' in x:
         from sklearn.linear_model import LinearRegression
         return LinearRegression()
