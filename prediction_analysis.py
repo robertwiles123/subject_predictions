f273d3de9d7ba9b28af5016f2a537ac9c26b903b
Robert Wiles <robertwiles123@gmail.com>
2023-10-17 12:23:31 +0000

    Updatd predction_analysis to use MAE isntead of MSE and removed graph

--- prediction_analysis.py
+++ prediction_analysis.py
@@ -11,7 +11,7 @@ df = pd.read_csv(f'{model}_scores/scores.csv')
 
 # Extract subject names, MSE, and RMSE values from the DataFrame
 subjects = df['subject']
-mse_values = df['MSE']
+mae_values = df['Mean Absolute Error']
 rmse_values = df['RMSE']
 
 # Create a figure with a larger size
@@ -24,7 +24,7 @@ ax = plt.subplot(111)
 bar_width = 0.4
 
 # Plot MSE bars in blue
-mse_bars = ax.bar(np.arange(len(subjects)), mse_values, width=bar_width, label='MSE', color='blue')
+mae_bars = ax.bar(np.arange(len(subjects)), mae_values, width=bar_width, label='MAE', color='blue')
 
 # Plot RMSE bars in red, adjust the x-positions
 rmse_bars = ax.bar(np.arange(len(subjects)) + bar_width, rmse_values, width=bar_width, label='RMSE', color='red')
@@ -36,10 +36,10 @@ ax.set_xticklabels(subjects, rotation=45, ha='right')
 # Add labels, title, and legend
 ax.set_xlabel('Subjects')
 ax.set_ylabel('Error Values')
-ax.set_title(f'{model.capitalize()} MSE and RMSE by Subject')
+ax.set_title(f'{model.capitalize()} MAE and RMSE by Subject')
 ax.legend()
 # line to show average MSE
-plt.axhline(y=1.5414, color='blue', linestyle='--', label='Teacher MSE')
+plt.axhline(y=0.8603, color='blue', linestyle='--', label='Teacher MAE')
 plt.axhline(y=1.1838, color='red', linestyle='--', label='Teacher RMSE')
 
 # Show the plot
@@ -84,36 +84,6 @@ print('Fig2 saved')
 
 plt.clf
 
-# Extract subject names and MCVS scores from the DataFrame
-subjects = df['subject']
-r2_scores = df['Mean cross validation']
-
-# Create an array of indices for the x-axis
-x = range(len(subjects))
-
-# Create the bar chart for MCVS scores
-fig, ax = plt.subplots(figsize=(15, 10))  # Adjust the figsize as needed
-
-# Plot MCSV scores in a single color (e.g., blue)
-r2_bars = ax.bar(x, r2_scores, color='blue')
-
-# Set the x-axis labels to be the subject names
-ax.set_xticks(x)
-ax.set_xticklabels(subjects, rotation=45, ha='right')
-
-# Add labels, title, and ylabel
-ax.set_xlabel('Subjects')
-ax.set_ylabel('Mean cross validation Score')
-ax.set_title(f'{model.capitalize()} mean cross validation Score by Subject')
-
-# Adjust the layout
-plt.grid(True, linestyle='--', alpha=0.6)
-plt.tight_layout()
-plt.savefig(f'analysis_graphs/{model} MCVS by subject.png')
-print('Fig3 saved')
-
-plt.clf
-
 # extract a string of numbers in to floats
 def extract_and_convert(s):
     s = s.strip('[]')  # Remove square brackets
@@ -138,5 +108,5 @@ plt.tight_layout()
 plt.grid(True, linestyle='--', alpha=0.6)
 plt.yticks(np.arange(0, 1.1, 0.1))
 plt.savefig(f'analysis_graphs/{model} cross-val range by subject.png')
-print('Fig4 saved')
+print('Fig3 saved')
 plt.clf
